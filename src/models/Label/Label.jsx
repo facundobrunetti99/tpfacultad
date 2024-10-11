0f9f7704106ffd4fb9c38e5b5d89475663b4337a
Facundo Brunetti <facundobrunetti99@gmail.com>
2024-10-11 08:01:19 -0300

    fbtp1-0

--- src/models/Label/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Label.css'
-const Label = ({labelValue}) => {
-  return (
-    <label className='input-label'>{labelValue}</label>
-  )
-}
-
-export default Label
